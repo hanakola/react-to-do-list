e9afb6d5dbcd28426eb2ef37184840d486a5785f
Hana K <66429579+hanakola@users.noreply.github.com>
2020-09-04 16:13:46 +0100

    Added form for adding new tasks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ function Todo({ todo, index }) {
   return <div className="Todo">{todo.text}</div>;
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  }
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add a new task"
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
 export default function App() {
   const [todos, setTodos] = useState([
     {
@@ -21,6 +47,11 @@ export default function App() {
     },
   ]);
 
+  function addTodo(text) {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
       <h1>To Do List</h1>
@@ -28,6 +59,7 @@ export default function App() {
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
