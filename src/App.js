e37876188194257e51f51e4d52c52969baf02247
Hana K <66429579+hanakola@users.noreply.github.com>
2020-09-04 14:31:23 +0100

    Created basic to do list with styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+function Todo({ todo, index }) {
+  return <div className="Todo">{todo.text}</div>;
+}
+
 export default function App() {
-  return <div className="App">To Do List</div>;
+  const [todos, setTodos] = useState([
+    {
+      text: "Go shopping",
+      isCompleted: false,
+    },
+    {
+      text: "Exercise",
+      isCompleted: false,
+    },
+    {
+      text: "Meeting with a friend",
+      isCompleted: true,
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <div className="to-do-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
 }
